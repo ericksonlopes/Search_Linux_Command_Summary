14c8f7011679a684e2b43b9df75140fd8444a236
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-13 19:33:21 -0300

    criando classe para database

--- database.py
+++ database.py
@@ -1,48 +1,66 @@
+import os
 import sqlite3
-
+from time import sleep
 from web_scraping import scraping_summary
 
-name_db = 'database.db'
 
-# tenta conectar a base de dados
-conn = sqlite3.connect('database.db')
+class DataBase:
+    def __init__(self):
+        name_db = 'database.db'
+
+        # tenta conectar a base de dados
+        self.conn = sqlite3.connect(name_db)
+
+        if not os.path.exists(name_db):
+            try:
+                self.create_table()
+            except Exception as error:
+                print(error)
+
+    # if not os.path.exists('database.db'):
+    def create_table(self):
+        """ Cria a tabela pricipal """
+        # Executa a criação da tabela no banco de dados
+        self.conn.execute("""
+        CREATE TABLE IF NOT EXISTS summary (
+            id integer primary key,
+            command text,
+            overview text,
+            link text
+        )
+        """)
+        # Comita as alterações no banco de dados
+        self.conn.commit()
+
+        # executa a função para adicionar todos os itens do sumário dentro da bd
+        self.add_items_summary()
+
+    # Adicionar todos os itens do sumário dentro da bd
+    def add_items_summary(self):
+        # instancia um cursos
+        cursor = self.conn.cursor()
+        # Sql para adicionar itens dentro da base de dados
+        sql = 'INSERT INTO summary (command, overview, link) VALUES (?, ?, ?)'
+        # Carrega os itens recebidos da função de scraping
+        val = scraping_summary()
+        # Realiza a query de inserção de todos os itens da lista
+        cursor.executemany(sql, val)
+        # Comita dentro da base de dados, salvando as alterações
+        self.conn.commit()
+        # Exibe o total de linhas afetadas
+        print(cursor.rowcount, 'Linha(s) afetada(s)')
+
+    def select_all(self):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM summary')
+        result = cursor.fetchall()
+
+        for item in result:
+            print(item)
 
 
-def create_table():
-    """ Cria a tabela pricipal """
-    # Executa a criação da tabela no banco de dados
-    conn.execute("""
-    CREATE TABLE IF NOT EXISTS summary (
-        id integer primary key,
-        command text,
-        overview text,
-        link text
-    )
-    """)
-    # Comita as alterações no banco de dados
-    conn.commit()
+# create_table()
 
-    # executa a função para adicionar todos os itens do sumário dentro da bd
-    add_items_summary()
+db = DataBase()
 
-
-# Adicionar todos os itens do sumário dentro da bd
-def add_items_summary():
-    # instancia um cursos
-    cursor = conn.cursor()
-    # Sql para adicionar itens dentro da base de dados
-    sql = 'INSERT INTO summary (command, overview, link) VALUES (?, ?, ?)'
-    # Carrega os itens recebidos da função de scraping
-    val = scraping_summary()
-    # Realiza a query de inserção de todos os itens da lista
-    cursor.executemany(sql, val)
-    # Comita dentro da base de dados, salvando as alterações
-    conn.commit()
-    print(cursor.rowcount, 'Linha(s) afetada(s)')
-
-
-def add_item():
-    pass
-
-
-create_table()
+db.select_all()
