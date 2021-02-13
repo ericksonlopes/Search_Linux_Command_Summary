dddc9a1a5a9d79c68d09c1598e59ed8a215ba603
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-13 18:25:20 -0300

    Criando tabela para os dados e documentando

--- database.py
+++ database.py
@@ -1,2 +1,20 @@
 import sqlite3
 
+conn = sqlite3.connect('database.db')
+
+
+def create_table():
+    """ Cria a tabela pricipal """
+    cursor = conn.cursor()
+
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS summary (
+        id integer primary key,
+        command text,
+        overview text,
+        link text
+    )
+    """)
+    conn.commit()
+
+create_table()
--- web_scraping.py
+++ web_scraping.py
@@ -6,15 +6,20 @@ url_summary = 'https://guialinux.uniriotec.br'
 # Requesição GET na url da página
 req_get = requests.get(url_summary)
 
+# Converte o html em obj python para manipulação
 soup = BeautifulSoup(req_get.content, 'html.parser')
 
+# Procura por todas as tags 'td', após tag 'a' de cada uma e retira o texto
 commands = [link.find('a').text for link in soup.find_all('dt')]
 
+# Procura por todas as tags 'td', após, tag 'a' de cada uma e retira o link(href)
 links = [link.find('a').attrs['href'] for link in soup.find_all('dt')]
 
-Summary = [item.text for item in soup.find_all('dd')]
+# Procura por todas as tags 'dd' e retira o texto contido em cada uma delas
+overview = [item.text for item in soup.find_all('dd')]
 
-join_lists = list(zip(links, commands, Summary))
+
+join_lists = list(zip(commands, overview, links))
 
 # print(len(link), len(comando))
 
