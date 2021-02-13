191fa14677adc04548697e061381b3345df4fb48
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-13 19:06:53 -0300

    comentando e criando cursor para alterações dentro da base

--- database.py
+++ database.py
@@ -21,16 +21,24 @@ def create_table():
     """)
     # Comita as alterações no banco de dados
     conn.commit()
+
+    # executa a função para adicionar todos os itens do sumário dentro da bd
     add_items_summary()
 
 
+# Adicionar todos os itens do sumário dentro da bd
 def add_items_summary():
+    # instancia um cursos
+    cursor = conn.cursor()
+    # Sql para adicionar itens dentro da base de dados
     sql = 'INSERT INTO summary (command, overview, link) VALUES (?, ?, ?)'
+    # Carrega os itens recebidos da função de scraping
     val = scraping_summary()
-
-    conn.executemany(sql, val)
-
+    # Realiza a query de inserção de todos os itens da lista
+    cursor.executemany(sql, val)
+    # Comita dentro da base de dados, salvando as alterações
     conn.commit()
+    print(cursor.rowcount, 'Linha(s) afetada(s)')
 
 
 def add_item():
