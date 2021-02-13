cc246490b90107e9141716278c60ec818e7fa22f
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-13 19:02:15 -0300

    finalizando scraping do sumario E inserindo itens dentro da base de dados

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
 import sqlite3
 
+from web_scraping import scraping_summary
+
+name_db = 'database.db'
+
+# tenta conectar a base de dados
 conn = sqlite3.connect('database.db')
 
 
 def create_table():
     """ Cria a tabela pricipal """
-    cursor = conn.cursor()
-
+    # Executa a criação da tabela no banco de dados
     conn.execute("""
     CREATE TABLE IF NOT EXISTS summary (
         id integer primary key,
@@ -15,6 +19,22 @@ def create_table():
         link text
     )
     """)
+    # Comita as alterações no banco de dados
     conn.commit()
+    add_items_summary()
+
+
+def add_items_summary():
+    sql = 'INSERT INTO summary (command, overview, link) VALUES (?, ?, ?)'
+    val = scraping_summary()
+
+    conn.executemany(sql, val)
+
+    conn.commit()
+
+
+def add_item():
+    pass
+
 
 create_table()
--- web_scraping.py
+++ web_scraping.py
@@ -1,30 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_summary = 'https://guialinux.uniriotec.br'
 
-# Requesição GET na url da página
-req_get = requests.get(url_summary)
+# Faz a busca dos dados dentro do sumario
+def scraping_summary():
+    url_summary = 'https://guialinux.uniriotec.br'
 
-# Converte o html em obj python para manipulação
-soup = BeautifulSoup(req_get.content, 'html.parser')
+    # Requesição GET na url da página
+    req_get = requests.get(url_summary)
 
-# Procura por todas as tags 'td', após tag 'a' de cada uma e retira o texto
-commands = [link.find('a').text for link in soup.find_all('dt')]
+    # Converte o html em obj python para manipulação
+    soup = BeautifulSoup(req_get.content, 'html.parser')
 
-# Procura por todas as tags 'td', após, tag 'a' de cada uma e retira o link(href)
-links = [link.find('a').attrs['href'] for link in soup.find_all('dt')]
+    # Procura por todas as tags 'td', após tag 'a' de cada uma e retira o texto
+    commands = [link.find('a').text for link in soup.find_all('dt')]
 
-# Procura por todas as tags 'dd' e retira o texto contido em cada uma delas
-overview = [item.text for item in soup.find_all('dd')]
+    # Procura por todas as tags 'td', após, tag 'a' de cada uma e retira o link(href)
+    links = [link.find('a').attrs['href'] for link in soup.find_all('dt')]
 
+    # Procura por todas as tags 'dd' e retira o texto contido em cada uma delas
+    overview = [item.text for item in soup.find_all('dd')]
 
-join_lists = list(zip(commands, overview, links))
+    # concatena as 3 listas em uma lsita nesse formato [(comando,lista,link), ...]
+    join_lists = list(zip(commands, overview, links))
 
-# print(len(link), len(comando))
-
-for item in join_lists:
-    print(item)
-
-
-print(len(join_lists))
+    # retorna a lista com os itens coletados do sumario
+    return join_lists
